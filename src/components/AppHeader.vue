196becbb7d27c959906d80f6322592b6cf8ca8f9
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-04-11 17:28:55 +0200

    add : header link v-for

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,52 @@
 <script>
 export default {
-   name:'AppHeader'
+   name:'AppHeader',
+   data(){
+    return {
+       links:[
+        {
+            text:'characters',
+            active:false
+        },
+        {
+            text:'comics',
+            active:true
+        },
+        {
+            text:'movies',
+            active:false
+        },
+        {
+            text:'tv',
+            active:false
+        },
+        {
+            text:'games',
+            active:false
+        },
+        {
+            text:'collectibles',
+            active:false
+        },
+        {
+            text:'videos',
+            active:false
+        },
+        {
+            text:'fans',
+            active:false
+        },
+        {
+            text:'news',
+            active:false
+        },
+        {
+            text:'shop',
+            active:false
+        },
+       ]
+    }
+   }
 }
 </script>
 
@@ -13,16 +59,7 @@ export default {
             </div>
             <nav>
                 <ul>
-                    <li><a href="#">characters</a></li>
-                    <li><a href="#">Comics</a></li>
-                    <li><a href="#">Movies</a></li>
-                    <li><a href="#">Tv</a></li>
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">Collectibles</a></li>
-                    <li><a href="#">Videos</a></li>
-                    <li><a href="#">Fans</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">Shop</a></li>
+                    <li :class="{'active': link.active  }"  v-for="link in links" ><a href="#">{{ link.text }}</a></li>
                 </ul>
             </nav>
         </div>
@@ -31,10 +68,10 @@ export default {
 </template>
 
 <style scoped lang="scss">
+@use '../style/partials/-variables' as *;
    .container {
-
         .row {
-            border: 1px solid black;
+           
             padding: 15px 0;
             display: flex;
             justify-content: space-between;
@@ -50,7 +87,30 @@ export default {
                 ul{
                     display: flex;
                     justify-content: flex-end;
-                    gap: 10px;
+                    gap: 25px;
+
+                    li {
+                        position: relative;
+                        a{
+                            color:#464646 ;
+                            
+                        }
+                       &.active ::before{
+                          content: '';
+                          width: 100%;
+                          position: absolute;
+                          height: 4px;
+                          bottom: -50px;
+                          
+                          background-color:$brand_primary ;
+                          
+                       }
+                      &.active{
+                        a{
+                            color:$brand_primary;
+                        }
+                      }
+                    }
                 }
               }
 
