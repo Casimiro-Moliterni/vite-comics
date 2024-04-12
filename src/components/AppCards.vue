a18bef16ace5f4254d79a180cd06963ff758eb80
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-04-12 15:25:52 +0200

    add: jumbotron image

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -5,18 +5,42 @@ export default {
 </script>
 
 <template>
-   <section >
+   <section  class="jumbo">
       <div class="container">
-          <div class="row">
-ssssssssss
-          </div>
+        <div class="text-serie">CURRENT SERIES</div>
       </div>
+   </section>
+   <section class="card-section">
+
    </section>
 </template>
 
 <style scoped lang="scss">
-     section{
-        height: 130px;
-        background-color: #1c1c1c;
+@use '../style/partials/-variables' as *;
+     .jumbo{
+        background-image: url('../assets/img/jumbotron.jpg');
+        background-size: cover;
+
+        .container{
+         position: relative;
+        min-height:400px;
+          
+          .text-serie{ 
+            color: white;
+            background-color: $brand_primary ;
+            padding: 10px 30px;
+            font-size: 18px;
+            font-weight: 600;
+            position: absolute;
+            bottom: -20px;
+            left: 0;
+          }
+        }
+
+     }
+
+     .card-section{
+      height: 130px;
+      background-color: #1c1c1c;
      }
 </style>
